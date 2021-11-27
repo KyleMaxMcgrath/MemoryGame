70574d938f3fd87f30ef7d3a74cca7938d811acc
Kyle McGrath <mcgratkm196@potsdam.edu>
2021-11-27 15:43:55 -0500

    Complete basic requirements.

---
+++ script.js
@@ -0,0 +1,106 @@
+const gameContainer = document.getElementById("game");
+
+const COLORS = [
+  "red",
+  "blue",
+  "green",
+  "orange",
+  "purple",
+  "red",
+  "blue",
+  "green",
+  "orange",
+  "purple",
+  "black",
+  "black"
+];
+
+let count = 0;
+
+let firstColor = "";
+let secondColor = "";
+
+active = [];
+
+// here is a helper function to shuffle an array
+// it returns the same array with values shuffled
+// it is based on an algorithm called Fisher Yates if you want ot research more
+function shuffle(array) {
+  let counter = array.length;
+
+  // While there are elements in the array
+  while (counter > 0) {
+    // Pick a random index
+    let index = Math.floor(Math.random() * counter);
+
+    // Decrease counter by 1
+    counter--;
+
+    // And swap the last element with it
+    let temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
+
+  return array;
+}
+
+let shuffledColors = shuffle(COLORS);
+
+// this function loops over the array of colors
+// it creates a new div and gives it a class with the value of the color
+// it also adds an event listener for a click for each card
+function createDivsForColors(colorArray) {
+  for (let color of colorArray) {
+    // create a new div
+    const newDiv = document.createElement("div");
+
+    // give it a class attribute for the value we are looping over
+    newDiv.classList.add(color);
+
+    // call a function handleCardClick when a div is clicked on
+    newDiv.addEventListener("click", handleCardClick);
+
+    // append the div to the element with an id of game
+    gameContainer.append(newDiv);
+  }
+}
+
+// TODO: Implement this function!
+function handleCardClick(event) {
+  // you can use event.target to see which element was clicked
+
+  let color = event.target.classList[event.target.classList.length-1];
+  event.target.style = `background-color: ${color};`;
+  if(firstColor === "")
+    firstColor = color;
+  else
+    secondColor = color;
+  if(firstColor !== "" && secondColor !== "" && firstColor === secondColor) {
+    let tiles = document.querySelectorAll(`.${firstColor}`);
+    for(let tile of tiles) {
+      tile.removeEventListener("click", handleCardClick);
+    }
+    firstColor = "";
+    secondColor = "";
+    count += 2;
+  }
+  if(firstColor !== "" && secondColor !== "" && firstColor !== secondColor) {
+    let tiles = document.querySelectorAll(`.${firstColor}`);
+    for(let tile of tiles) {
+      tile.style = "background-color: white;";
+    }
+    tiles = document.querySelectorAll(`.${secondColor}`);
+    for(let tile of tiles) {
+      tile.style = "background-color: white;";
+    }
+    firstColor = "";
+    secondColor = "";
+  }
+  if(count == COLORS.length)
+    alert("You won!");
+  console.log("you just clicked", event.target);
+}
+
+// when the DOM loads
+createDivsForColors(shuffledColors);
